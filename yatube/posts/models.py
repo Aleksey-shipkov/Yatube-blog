3c4e512c5bc5b56b071f9071bdd3127a7dcd0099
Anya S <shipkov.alexey@gmail.com>
2022-10-28 13:21:51 +0300

    Added new file

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -85,7 +85,7 @@ class Follow(models.Model):
     class Meta:
         constraints = (
             models.UniqueConstraint(
-                fields=('user', 'author'), name='unique_following'))
+                fields=('user', 'author'), name='unique_following'),)       
 
     def __str__(self) -> str:
         return f'{self.user.username} подписан на {self.author.username}'
